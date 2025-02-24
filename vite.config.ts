994946a291f393571dce02dd0e6b10981eebf981
nj22az <nils.johansson@me.com>
2025-02-24 09:12:02 +0100

    Update base path for new repository name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/personalized-network-space/',
+  base: '/theofficeofnilsjohansson/',
   server: {
     host: "::",
     port: 8080,
